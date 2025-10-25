390962f236c387e124ae64866214806349f9f7c3
Mads Voss Hvelplund <mads.hvelplund@gmail.com>
2025-10-25 17:25:16 +0200

    clients can now write and receive messages from eachother

--- client/client.go
+++ client/client.go
@@ -11,6 +11,26 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+func messageWritingLoop(client proto.ChitChatClient) {
+	var content string
+	for {
+		fmt.Scanln(&content)
+		client.PostMessage(context.Background(), &proto.ChatMsg{Text: content})
+	}
+}
+
+func messageReceivingLoop(stream grpc.ServerStreamingClient[proto.ChatMsg]) {
+	for {
+		msg, err := stream.Recv()
+		if msg != nil {
+			fmt.Println(msg.Text)
+		}
+		if err == nil {
+		}
+		time.Sleep(time.Millisecond * 100)
+	}
+}
+
 func main() {
 	conn, err := grpc.NewClient("localhost:5050", grpc.WithTransportCredentials(insecure.NewCredentials())) //connects to server at localhost:5050. Insecure.newcredentials is used to skip TLS encryption for simplification
 	if err != nil {
@@ -19,21 +39,14 @@ func main() {
 
 	client := proto.NewChitChatClient(conn) //creates a client that can call RPC methods defined in the ChitChat service.
 	// Establish connection stream to recaive messages from server
-	connection, err := client.ServerStream(context.Background(), &proto.Chat_Request{Greeting: "sup"})
-	client.PostMessage(connection.Context(), &proto.ChatMsg{Text: "hello guys"})
+	stream, err := client.ServerStream(context.Background(), &proto.Chat_Request{Greeting: "sup"})
 	if err != nil {
 		log.Fatalf("Connection was not established")
 	}
-	for {
-		msg, err := connection.Recv()
-		if msg != nil {
-			fmt.Println(msg.Text)
-		}
-		if err == nil {
-		}
-		time.Sleep(time.Millisecond * 100)
 
-		//client.PostMessage(context.Background(), &proto.ChatMsg{Text: "hey guys"})
+	go messageWritingLoop(client)
+	go messageReceivingLoop(stream)
+	for { // connection lifetime loop
 	}
 
 }
